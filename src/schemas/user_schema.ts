c2b84a24c0e99f236af1ba50103588bae351fbef
Fabrice <codinglone@gmail.com>
2023-04-25 20:13:17 +0200

    Changes on create user

--- src/schemas/user_schema.ts
+++ src/schemas/user_schema.ts
@@ -2,6 +2,18 @@ import { Type as T } from "@sinclair/typebox";
 import { createAccountController } from "../controllers/create_account_controller";
 
 
+export const getUserSchema = T.Object({
+    id: T.Number(),
+    firstName: T.String(),
+    lastName: T.String(),
+    email: T.String(),
+    password: T.String(),
+    role: T.String(),
+    phone: T.String(),
+    createdAt: T.String(),
+    updatedAt: T.String()
+})
+
 export const accountSchema = T.Object({
     firstName: T.String(),
     lastName: T.String(),
@@ -15,7 +27,7 @@ export const postUserOpts = {
     schema: {
         body: T.Strict(accountSchema),
         response: {
-            201: accountSchema
+            201: getUserSchema
         }
     },
     handler: createAccountController
