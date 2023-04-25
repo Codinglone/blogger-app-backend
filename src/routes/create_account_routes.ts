8b2597ca00ee53f862460d1c2b87a55fd4acc6e0
Fabrice <codinglone@gmail.com>
2023-04-25 19:34:21 +0200

    Added userRoutes function

---
+++ src/routes/create_account_routes.ts
@@ -0,0 +1,11 @@
+import fastify = require("fastify");
+import { postUserOpts } from "../schemas/user_schema";
+import { bloggerRoutes } from "../utils/enums";
+
+
+const userRoutes = (fastify, opts, done) => {
+    fastify.post(`${bloggerRoutes.ACCOUNT_CREATION}`, postUserOpts)
+    done()
+}
+
+module.exports = userRoutes
