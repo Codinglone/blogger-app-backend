aceb2d653cc046c2f4336171c1ff2eb352aaa09e
Fabrice <codinglone@gmail.com>
2023-04-25 18:32:24 +0200

    Created AccrountCreationApi class

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -4,3 +4,12 @@ import { UserPayload } from "../utils/interfaces";
 const jwt = require("jsonwebtoken")
 
 const bcrypt = require("bcrypt")
+
+export class AccountCreationApi {
+    static async createUser(payload: UserPayload): Promise<any> {
+        const userRepository = AppDataSource.getRepository(User)
+        
+    }
+}
+
+
