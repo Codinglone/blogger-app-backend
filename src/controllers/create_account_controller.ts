5bd4188002272a09b72bd873fcdd2f5b833575c9
Fabrice <codinglone@gmail.com>
2023-04-25 19:02:24 +0200

    Added createAccountController

---
+++ src/controllers/create_account_controller.ts
@@ -0,0 +1,10 @@
+import { AccountCreationApi } from "../models/users.model";
+
+const createAccountController = async (req, reply) => {
+    try {
+        const response = AccountCreationApi.createUser(req.body)
+        reply.code(201).send(response)
+    } catch (error) {
+        reply.code(204).send(error)
+    }
+}
