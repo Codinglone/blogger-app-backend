2d63eb3e346525a76b89e67603227eb230a753bd
Fabrice <codinglone@gmail.com>
2023-04-25 19:23:05 +0200

    Added accountCreationSchema

--- src/schemas/user_schema.ts
+++ src/schemas/user_schema.ts
@@ -1,1 +1,10 @@
 import { Type as T } from "@sinclair/typebox";
+
+const accountSchema = T.Object({
+    firstName: T.String(),
+    lastName: T.String(),
+    email: T.String(),
+    password: T.String(),
+    role: T.String(),
+    phone: T.String()
+})
