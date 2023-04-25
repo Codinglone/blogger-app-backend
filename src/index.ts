df42d1424c73f5efd28f884370fe816dbc4bfdcc
Fabrice <codinglone@gmail.com>
2023-04-25 19:42:35 +0200

    Added fastify route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
+require("dotenv").config()
 import { AppDataSource } from "./data-source"
 import { User } from "./entity/User"
 const fastify = require('fastify')({ logger: true })
+const PORT = process.env.PORT || 7000
+
+fastify.register(require("@fastify/swagger"), {
+    exposeRoute: true,
+    routePrefix: "api-docs",
+    swagger: {
+        info: { 
+            title: "Blogger-Docs",
+            version: "1.0.0",
+            description: "Basic blogging app backend with authentication"
+
+         }
+    }
+})
+
 
 
 
 AppDataSource.initialize().then(async () => {
     const start = async () => {
         try {
-          await fastify.listen({ port: 3000 })
+          await fastify.listen({ port: PORT })
         } catch (err) {
           fastify.log.error(err)
           process.exit(1)
