76ba86f0f1fd9ff5fe47440635d19757532afccd
Fabrice <codinglone@gmail.com>
2023-04-25 18:27:57 +0200

    Renamed payload file to interfaces

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,2 +1,6 @@
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
+import { UserPayload } from "../utils/interfaces";
+const jwt = require("jsonwebtoken")
+
+const bcrypt = require("bcrypt")
