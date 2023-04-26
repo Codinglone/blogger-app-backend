96e9ec5f5683c8edee2e7029d19ff921b04be84e
Fabrice <codinglone@gmail.com>
2023-04-26 19:46:39 +0200

    Added checkauth middleware

---
+++ src/middlewares/checkauth.ts
@@ -0,0 +1,36 @@
+import { User } from "../entity/User";
+import { AppDataSource } from "../data-source";
+
+const jwt = require("jsonwebtoken")
+const checkauth = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization 
+        const token = authHeader.substring(7)
+        if(!token){
+            return res.code(401).send({ message: 'Access denied, Try Again!' })
+        }
+        else {
+            const verifyToken = jwt.verify(
+                authHeader.substring(7),
+                process.env.JWT_KEY
+            )
+
+            const userId = verifyToken.id 
+            const userRepository = AppDataSource.getRepository(User)
+            const findUser = await userRepository.findOneBy({
+                id: userId
+            })
+
+            if(findUser){
+                req.user = findUser
+            }
+            else {
+                res.code(401).send({ message: 'Token is not valid' })
+            }
+        }
+    } catch (error) {
+        res.code(401).send({ message: 'Wrong authentication', error })
+    }
+}
+
+export default checkauth
