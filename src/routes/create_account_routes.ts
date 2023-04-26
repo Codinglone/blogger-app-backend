d2899495fe7996e66bc98d7af54f6ddb855b1e6a
Fabrice <codinglone@gmail.com>
2023-04-26 18:54:25 +0200

    Added get users route

--- src/routes/create_account_routes.ts
+++ src/routes/create_account_routes.ts
@@ -1,9 +1,10 @@
-import { postUserOpts } from "../schemas/user_schema";
+import { postUserOpts, getUsersOpts } from "../schemas/user_schema";
 import { bloggerRoutes } from "../utils/enums";
 
 
 const userRoutes = (fastify, opts, done) => {
     fastify.post(`${bloggerRoutes.ACCOUNT_CREATION}`, postUserOpts)
+    fastify.get(`${bloggerRoutes.ACCOUNT_CREATION}`, getUsersOpts)
     done()
 }
 
